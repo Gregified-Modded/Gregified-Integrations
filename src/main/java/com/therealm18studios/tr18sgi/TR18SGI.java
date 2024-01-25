8e1e5e9fdbf428fb669c55dcdce28262a50812a7
TheRealM18 <github@therealm18.com>
2024-01-25 19:05:11 -0400

    Basic stuff

---
+++ src/main/java/com/therealm18studios/tr18sgi/TR18SGI.java
@@ -0,0 +1,16 @@
+package com.therealm18studios.tr18sgi;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(TR18SGI.MODID)
+public class TR18SGI
+{
+    public static final String MODID = "tr18sgi";
+
+    public TR18SGI()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    }
+}
