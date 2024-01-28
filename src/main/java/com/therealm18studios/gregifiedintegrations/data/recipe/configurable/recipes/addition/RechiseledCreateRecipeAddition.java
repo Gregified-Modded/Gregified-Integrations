8a61f278884a3511e9581e64be4ccc98f64b846b
TheRealM18 <github@therealm18.com>
2024-01-28 14:06:34 -0400

    new recipe for rechiseled create

--- src/main/java/com/therealm18studios/gregifiedintegrations/data/recipe/configurable/recipes/addition/RechiseledCreateRecipeAddition.java
+++ src/main/java/com/therealm18studios/gregifiedintegrations/data/recipe/configurable/recipes/addition/RechiseledCreateRecipeAddition.java
@@ -1,8 +1,12 @@
 package com.therealm18studios.gregifiedintegrations.data.recipe.configurable.recipes.addition;
 
+import com.gregtechceu.gtceu.common.data.GTBlocks;
+import com.gregtechceu.gtceu.common.data.GTItems;
 import com.gregtechceu.gtceu.data.recipe.VanillaRecipeHelper;
+import com.supermartijn642.rechiseled.Rechiseled;
 import com.supermartijn642.rechiseled.create.RechiseledCreate;
 import com.therealm18studios.gregifiedintegrations.config.GIConfigHolder;
+import com.therealm18studios.gregifiedintegrations.data.tags.GregTechCEuTags;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.world.item.ItemStack;
 
@@ -19,7 +23,7 @@ public class RechiseledCreateRecipeAddition {
 
         boolean nerfed = GIConfigHolder.INSTANCE.recipesAdditions.harderRechiseledCreateAddRecipes;
         if (nerfed) {
-//            VanillaRecipeHelper.addShapedRecipe(provider, "chisel", new ItemStack(RechiseledCreate.mechanical_chisel.getClass().get()), "sP", 'P', BlockRegistry.ARCHWOOD_PLANK);
+            VanillaRecipeHelper.addShapedRecipe(provider, "mechanical_chisel", new ItemStack(RechiseledCreate.mechanical_chisel), "ABC", "DED", 'A', GTItems.CONVEYOR_MODULE_LV, 'b', GTBlocks.MACHINE_CASING_LV, 'c', GTItems.ELECTRIC_MOTOR_LV, 'D', GregTechCEuTags.ModTags.GTCEU_CIRCUITS_LV, 'E', Rechiseled.chisel);
         } else {
 
         }
