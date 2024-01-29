17579dc94e717e9ad5b420069c3039b0e2b91045
TheRealM18 <github@therealm18.com>
2024-01-29 14:53:59 -0400

    removal for ars Énergistique

---
+++ src/main/java/com/therealm18studios/gregifiedintegrations/data/recipe/configurable/removal/ArsÉnergistiqueRecipeRemoval.java
@@ -0,0 +1,20 @@
+package com.therealm18studios.gregifiedintegrations.data.recipe.configurable.removal;
+
+import com.therealm18studios.gregifiedintegrations.config.GIConfigHolder;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.function.Consumer;
+
+public class ArsÉnergistiqueRecipeRemoval {
+
+    public static void init(Consumer<ResourceLocation> registry) {
+        if (GIConfigHolder.INSTANCE.recipesRemoval.a.harderAppliedMekanisticsRecipes) harderAppliedMekanisticsRecipes(registry);
+    }
+
+    private static void harderAppliedMekanisticsRecipes(Consumer<ResourceLocation> registry) {
+        registry.accept(new ResourceLocation("arseng:cable_source_acceptor"));
+        registry.accept(new ResourceLocation("arseng:source_acceptor"));
+        registry.accept(new ResourceLocation("arseng:source_acceptor_from_part"));
+        registry.accept(new ResourceLocation("arseng:source_cell_housing"));
+    }
+}
