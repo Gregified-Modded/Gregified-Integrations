b99e3cdfeb18db7bea180ae18f066fd8ebb6aa57
TheRealM18 <github@therealm18.com>
2024-02-01 10:12:50 -0400

    new implementation of tags

--- src/main/java/com/therealm18studios/gregifiedintegrations/data/tags/AppliedEnergistics2Tags.java
+++ src/main/java/com/therealm18studios/gregifiedintegrations/data/tags/AppliedEnergistics2Tags.java
@@ -1,8 +1,10 @@
 package com.therealm18studios.gregifiedintegrations.data.tags;
 
+import com.gregtechceu.gtceu.api.data.tag.TagUtil;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class AppliedEnergistics2Tags {
@@ -14,6 +16,7 @@ public class AppliedEnergistics2Tags {
         /**
          * Applied Energistics 2
          */
-        public static final TagKey<Block> APPLIEDENERGISTICS2_SMART_DENCE = BlockTags.create(new ResourceLocation("ae2", "smart_dense_cable"));
+        public static final TagKey<Item> APPLIEDENERGISTICS2_SMART_DENCE = TagUtil.createModItemTag("ae2/smart_dense_cable");
+        public static final TagKey<Item> APPLIEDENERGISTICS2_GLASS_CABLE = TagUtil.createModItemTag("ae2/glass_cable");
     }
 }
